acb7ba6345866f7d1359421b93ed250906198eca
eeedris <alabialade@gmail.com>
2022-03-07 03:14:34 +0100

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
-import HomePage from "./components/Homepage";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./components/homePage";
+import PollSetup from "./components/pollSetup";
 class App extends Component {
   state = {};
 
   render() {
-    return <HomePage />;
+    return (
+      <div className="content">
+        <Switch>
+          <Route path="/poll-setup" component={PollSetup}></Route>
+          <Route path="/" component={HomePage}></Route>
+        </Switch>
+      </div>
+    );
   }
 }
 
---
+++ client/src/components/pollSetup.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class PollSetup extends Component {
+  state = {};
+  render() {
+    return (
+      <main className="bg-bkblue w-screen h-screen">
+        <div className="w-11/12 h-5/6 bg-ablue mx-auto pt-6">Form</div>
+      </main>
+    );
+  }
+}
+
+export default PollSetup;
